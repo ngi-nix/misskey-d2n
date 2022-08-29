dd709e6a86db425de1aac37d2537b5d143121100
Mihai Fufezan <fufexan@protonmail.com>
2022-08-29 16:00:37 +0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    dream2nix.url = "github:nix-community/dream2nix";
+    misskey = {
+      url = "github:misskey-dev/misskey?submodules=true";
+      flake = false;
+    };
+  };
+
+  outputs = {self, nixpkgs, dream2nix, misskey}:
+    dream2nix.lib.makeFlakeOutputs {
+      systems = ["x86_64-linux"];
+      config.projectRoot = ./.;
+      packageOverrides = {};
+      source = misskey;
+    };
+}
