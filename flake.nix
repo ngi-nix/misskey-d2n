2b57c4cc56e5216308b6b8f84d120b5c28389c17
Mihai Fufezan <fufexan@protonmail.com>
2022-09-05 13:15:14 +0300

    Try to override os-locale

--- flake.nix
+++ flake.nix
@@ -5,18 +5,36 @@
     dream2nix.inputs.nixpkgs.follows = "nixpkgs";
 
     misskey = {
-      url = "github:misskey-dev/misskey?submodules=true";
+      url = "github:misskey-dev/misskey/12.118.1?submodules=true";
       flake = false;
     };
   };
 
-  outputs = {self, nixpkgs, dream2nix, misskey}:
+  outputs = {
+    self,
+    nixpkgs,
+    dream2nix,
+    misskey,
+  }:
     (dream2nix.lib.makeFlakeOutputs {
       systems = ["x86_64-linux"];
       config.projectRoot = ./.;
-      packageOverrides = {};
+      settings = [{translator = "yarn-lock";}];
       source = misskey;
-    }) // {
+      inject = {
+        misskey."12.118.1" = [
+          ["os-locale" "1.4.0"]
+        ];
+      };
+      sourceOverrides = oldSources: {
+        os-locale."1.4.0" = builtins.fetchTarball {
+          # name = "os-locale-npm-1.4.0-924760b837-0161a1b6b5.zip";
+          url = "https://registry.yarnpkg.com/os-locale/-/os-locale-1.4.0.tgz#20f9f17ae29ed345e8bde583b13d2009803c14d9";
+          sha256 = "";
+        };
+      };
+    })
+    // {
       nixosModules.misskey = import ./nixosModule self;
       nixosModules.default = self.nixosModules.misskey;
     };
